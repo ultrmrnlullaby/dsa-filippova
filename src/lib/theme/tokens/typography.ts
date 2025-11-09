2289258663942951bec78a87735b7addb6c58380
Anastasia F <ultrmrnlullaby@gmail.com>
2025-11-09 17:02:14 +0700

    Добавлены токены colors, spacers, typography

--- src/lib/theme/tokens/typography.ts
+++ src/lib/theme/tokens/typography.ts
@@ -2,9 +2,9 @@ import { spacers } from "./spacers";
 
 export const typography = {
 	fontFamily: {
-		base: `"Inter", -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
-		heading: `"Inter", -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
-		monospace: `'SFMono-Medium', 'SF Mono', 'Segoe UI Mono', 'Roboto Mono', 'Ubuntu Mono', Menlo, Consolas, Courier, monospace`,
+		base: `"Roboto", -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
+		heading: `"Roboto", -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
+		monospace: `'Space Mono', 'SF Mono', 'Segoe UI Mono', 'Roboto Mono', 'Ubuntu Mono', Menlo, Consolas, Courier, monospace`,
 	},
 
 	fontSize: {
@@ -12,23 +12,25 @@ export const typography = {
 		h2: spacers[40],
 		h3: spacers[32],
 		h4: spacers[24],
-		h5: spacers[18],
+		h5: spacers[20],
 		h6: spacers[16],
-		base: spacers[16],
-		small: spacers[14],
-		extraSmall: spacers[12],
+		body: spacers[16],
+		smallBody: spacers[14],
+		smallText: spacers[12],
+		extraSmallText: spacers[10],
 	},
 
 	lineHeight: {
-		h1: spacers[56],
-		h2: spacers[40],
-		h3: spacers[32],
-		h4: spacers[24],
-		h5: spacers[24],
+		h1: spacers[60],
+		h2: spacers[48],
+		h3: spacers[36],
+		h4: spacers[32],
+		h5: spacers[28],
 		h6: spacers[24],
-		base: spacers[24],
-		small: spacers[20],
-		extraSmall: spacers[16],
+		body: spacers[24],
+		smallBody: spacers[20],
+		smallText: spacers[16],
+		extraSmallText: spacers[12],
 	},
 
 	fontWeight: {
